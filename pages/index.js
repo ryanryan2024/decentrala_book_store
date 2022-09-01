7f7365526539bbe5bb8120adcb73688e87d363f5
Ryan <76850473+ryanryan2024@users.noreply.github.com>
2022-09-01 06:10:29 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -23,8 +23,8 @@ export default function Home() {
             <div className='p-10 font-extralight flex justify-center'>
                 Diversed books, diversed audience.
             </div>
-            <div className='font-extralight flex justify-center text-sm'>
-                Note: Want to exchange with a different currency? <a className="underline" href="https://decentrala.netlify.app/contact.html">contact us.</a>
+            <div className='font-extralight flex justify-center text-sm pb-5'>
+                Note: Want to exchange with a different currency? <a className="underline" href="https://decentrala.netlify.app/contact.html"> contact us.</a>
             </div>
             <Tab.Group>
                 <Tab.List className="flex justify-between px-5 bg-white text-black p-3 rounded-3xl text-sm">
